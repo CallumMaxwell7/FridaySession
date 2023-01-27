6f14eaa3a245abe6ff66633129fe195238aeea7e
CallumMaxwell7 <123733328+CallumMaxwell7@users.noreply.github.com>
2023-01-27 10:51:58 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,123 @@
+// console.log("All Around The World".charAt(7).toUpperCase());
+
+// let firstName = "Callum";
+// const secondName = "Maxwell";
+// firstName= "Steve"
+// console.log(firstName);
+// console.log(secondName);
+
+//let num1=5
+//let num2=7
+//let result =num1+num2
+//console.log(result)
+//num2+= num1
+//console.log(num2)
+//let favouriteDrink ="Tango"
+//let favouriteSnack ="Pies"
+//console.log(`My favourite drink is , ${favouriteDrink}, and my favorite snack is ${favouriteSnack}`);
+//console.log("My favourite drink is " + favouriteDrink)
+
+//  let firstName ="Callum"
+//  let age ="24"
+// let favouriteColour= "Blue"
+// let sentence =`My name is ${firstName} , My age is ${age}, and my favourite colour is ${favouriteColour}`;
+// console.log(sentence)
+
+// let newName="Bob"
+// let newAge="12"
+// let newFavouriteColour="Pink"
+// let newSentence=`My name is ${newName} , my age is ${newAge} , and my favourite colour is ${newFavouriteColour}`;
+// console.log(newSentence)
+// let breakfastToday ="toast"
+// let lunchToday ="sausage rolls"
+// let dinnerToday = "chicken pizza"
+//  let foodToday =`My breakfast today was ${breakfastToday} , for lunch today I am having ${lunchToday}, and for my dinner today I will be having ${dinnerToday}`
+// console.log(foodToday)
+
+//  let breakfastTomorrow="cereal"
+// let lunchTomorrow="sandwich"
+//  let dinnerTomorrow="cricken wraps"
+// let foodTomorrow= `My breakfast tomorrow is ${breakfastTomorrow}, for lunch I am having a ${lunchTomorrow}, and for my dinner I am having ${dinnerTomorrow}`
+// console.log(foodTomorrow)
+// let date1= new Date ("24/01/2023")
+// let date2 = new Date ("26/11/2023")
+// let differenceInTime = 
+// let weather ="rainy"
+// if (weather =="sunny"){
+//     console.log("get your shorts on")
+// }
+// else if(weather=="rainy"){
+//     console.log("it's raining")
+// }
+
+// else{
+//     console.log("who knows??")
+// }
+
+// if(1 !=="1"){
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+// let place ="Manchester"
+// let weather = "rainy"
+// if(place == "Manchester" && weather =="rainy "){
+//     console.log( "yeah that sounds like Manchester to me" )
+// }
+// else{
+//     console.log("I don't know where you are")
+// }
+// let age =19
+// if(age > 17){
+//      console.log("I can serve you")
+// }
+// else {   console.log("You are not old enough")
+// }
+// let pizzaTopping ="Chicken"
+// if(pizzaTopping==="Chicken"){
+//     console.log(`These ingredients are important for my pizza, I don't mind having ${pizzaTopping} on my pizza`)
+// }
+// else {
+//     console.log(`${pizzaTopping} does not belong on pizza`)
+
+// }
+// let password="WiganAthletic"
+// let letters=7
+// if(letters < 8 ){
+//    console.log("Your password is too short ")
+//  }
+// else{
+//    console.log(`Your password is long enough and it is ${password}`)
+// }
+//  let num= 14
+// if(num % 5 ===0|| num % 3===0){
+// console.log("the number is divisible by 3 or 5")
+// }
+//  else{
+//     console.log("the number is not divisible by 3 or 5")
+// }
+// let num=30
+// if(num %3===0){
+//     console.log("fizz")
+// }
+// if(num % 5=== 0){
+// console.log("buzz")
+// }
+// if(num % 3===0 && num %5===0){
+//     console.log("fizz buzz")
+// }
+//  if(num %3 !=0 || num %5 !=0)
+//     console.log(num, "this is not divisible by 3 or 5")
+// let num=1001
+// function checkPalindrome(num){
+//     const arrayValues =num.split("");
+//     const reversenumValues = arrayValues.reverse();
+//     const reverseNum = reversenumValues.join("");
+//     if(num ==reverseNum){
+//         console.log("it is a palindrome ")
+//     }
+//     else{
+//         console.log("it is not a palindrome")
+//     }
+// }
